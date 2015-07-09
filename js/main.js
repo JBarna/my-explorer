0ac3950b9cda1d283dad04adb1b1eb590c0f613f
Barna <barnaj@roselcdv0069204.es.ad.adp.com>
2015-07-09 16:01:11 -0400

    Refactored mywalk.js to be super fucking awesome.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,51 @@
 global.$ = $;
 
-var walk = require('mywalk');
+var DOMFactory = require('DOMFactory');
+var walker = require('mywalk');
 var path = require('path');
 
 $(global.window.document).ready(function(){
   
   var start = '/Users/barnaj/Desktop';
+  var walk = walker(start); 
   
   //scan current directory
-  var dirResults = walk.scanDir(start);
+  var dirResults = walk.scanDir();
   
   //manage files in current directory
   for (file of dirResults.files){
-    var $newFileElement = $('<div>').addClass('file').text(file);
-    $('.fileHolder').append($newFileElement);
+    $('.fileHolder').append(DOMFactory.file(file));
   }
   
   //manage subdirectories in current directory
   for (dir of dirResults.directories){
-    var $newChildFolder = $("<div>").addClass('child folderView').attr('id', dir).text(dir).append($("<br/>"));
-    $('#childSection').append($newChildFolder);
+    $('#childSection').append(DOMFactory.folderView(dir));
     
     //manage files in subdirectories
-    var childDirResults = walk.scanChildDir(start, dir);
+    var childDirResults = walk.scanChildDir(dir);
     for (file of childDirResults.files){
-      var newChildFile = $("<div>").addClass("file").text(file);
-      $('#' + dir).append(newChildFile);
+      $('#' + dir).append(DOMFactory.file(file));
     }
   }
   
   
   //scan parent directory
-  var parent = "/Users/barnaj/";
-  var parentResults = walk.scanDir(parent);
-  
-  console.log(parentResults);
+  var parentResults = walk.scanParentDir();
   
   //manage files in parent directory
   for (file of parentResults.files){
-    var $newFileElement = $('<div>').addClass('file').text(file);
-    $('#parentDirectory').append($newFileElement);
+    $('#parentDirectory').append(DOMFactory.file(file));
   }
   
   for (dir of parentResults.directories){
     //not our current directory
     if (dir !== path.basename(start) && !dir.startsWith('.')){
-      var $newChildFolder = $("<div>").addClass('sibling folderView').attr('id', dir).text(dir).append($("<br/>"));
-      $('#parentandsibs').append($newChildFolder);
+      $('#parentandsibs').append(DOMFactory.folderView(dir));
       
-      var siblingDirResults = walk.scanDir(path.join(parent, dir));
+      var siblingDirResults = walk.scanSiblingDir(dir);
       
       for (file of siblingDirResults.files){
-        var $newFileElement = $('<div>').addClass('file').text(file);
-        $('#' + dir).append($newFileElement);
+        $('#' + dir).append(DOMFactory.file(file));
       }
     }
     
