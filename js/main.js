abc5f7c1f3d101865676c2982909412a3c1bc292
Barna <barnaj@roselcdv0069204.es.ad.adp.com>
2015-07-09 12:14:43 -0400

    Got child directories and child files

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ global.$ = $;
 var walk = require('mywalk');
 
 $(global.window.document).ready(function(){
+  
+  
   var start = '/Users/barnaj/Desktop';
   walk.getFiles(start, function(fileStats){
     var $newFileElement = $('<div>').addClass('file').text(fileStats.name);
     $('.fileHolder').append($newFileElement);
   });
+  
+  var childHandle = walk.getChildDirectories(start, function(directoryStat){
+    console.log(directoryStat.name);
+    childHandle.getFiles(directoryStat, function(fileStats){
+      console.log(directoryStat.name + ": " + fileStats.name);
+    });
+  });
+    
+  
 });
   
   
