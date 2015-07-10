a8f9962f3dff120ee09cd53f0d6d1e36f4f180ed
Barna <barnaj@roselcdv0069204.es.ad.adp.com>
2015-07-10 16:49:17 -0400

    Something someting

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@ var DOMFactory = require('DOMFactory');
 var myfs = require('myfs');
 var path = require('path');
 
-$(global.window.document).ready(function(){
+$(window.document).ready(function(){
   
   
   var start = '/Users/barnaj/Desktop'
   myfs.cd(start);
   
+  window.ondragover = function(e) { e.preventDefault(); return false;  };
+  window.ondrop = function(e) { e.preventDefault(); return false; };
+  
+  var currentDir = window.document.getElementById('currentDirectory');
+  
+  currentDir.ondrop = function(e){
+    e.preventDefault();
+    console.log(e);
+    for (var i = 0; i < e.dataTransfer.files.length; i++)
+      console.log(e.dataTransfer.files[i]);
+    return false;
+  };
   
 });
   
