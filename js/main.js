853c78d750bf46c1e81bebf870fa0b11dfcf1e7a
Barna <barnaj@roselcdv0069204.es.ad.adp.com>
2015-07-13 12:25:30 -0400

    Added drag/drop

--- js/main.js
+++ js/main.js
@@ -2,26 +2,17 @@ global.$ = $;
 
 var DOMFactory = require('DOMFactory');
 var myfs = require('myfs');
+
 var path = require('path');
+var fs = require('fs');
+var handleEvents = require('eventHandler');
 
 $(window.document).ready(function(){
   
   
   var start = '/Users/barnaj/Desktop'
   myfs.cd(start);
-  
-  window.ondragover = function(e) { e.preventDefault(); return false;  };
-  window.ondrop = function(e) { e.preventDefault(); return false; };
-  
-  var currentDir = window.document.getElementById('currentDirectory');
-  
-  currentDir.ondrop = function(e){
-    e.preventDefault();
-    console.log(e);
-    for (var i = 0; i < e.dataTransfer.files.length; i++)
-      console.log(e.dataTransfer.files[i]);
-    return false;
-  };
+  handleEvents();
   
 });
   
